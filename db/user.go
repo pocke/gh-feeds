ac63760ab774e2d01e42b792e2185c3757af689f
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-10-13 23:01:36 +0900

    CreateUser func

--- db/user.go
+++ db/user.go
@@ -11,3 +11,21 @@ type User struct {
 }
 
 func (m User) hasManyEvents() *ar.Association { return nil }
+
+func CreateUser(p *UserParams) (*User, error) {
+	ins := ar.NewInsert(db, logger).Table("users").Params(map[string]interface{}{
+		"id":   p.ID,
+		"name": p.Name,
+		"auth": p.Auth,
+	})
+	if _, err := ins.Exec(); err != nil {
+		errs := &ar.Errors{}
+		errs.AddError("base", err)
+		return nil, errs
+	}
+	return &User{
+		ID:   p.ID,
+		Name: p.Name,
+		Auth: p.Auth,
+	}, nil
+}
